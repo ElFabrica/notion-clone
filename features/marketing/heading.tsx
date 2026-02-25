de7404f035e4a34e81ae4daade235e052e034aad
Arthur Fabricyo <arthur.fabricyo@gmail.com>
2026-02-25 09:12:48 -0300

    feat/update marketing

---
+++ features/marketing/heading.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { ArrowRightIcon } from "lucide-react";
+
+export function Heading() {
+  return (
+    <div className=" max-w-3xl space-y-4">
+      <h1 className="text--3xl sm:text-5xl md:text-6xl font-bold">
+        Your Ideas, Documents, & Plans. Unified. Welcome to{" "}
+        <span className="underline">Jotion</span>
+      </h1>
+      <h3 className="text-base sm:text-xl md:text-2xl font-medium">
+        Jotion is the connected workspacec where <br />
+        Better, faster work happens
+      </h3>
+      <Button>
+        Enter Jotion <ArrowRightIcon className="size-4" />
+      </Button>
+    </div>
+  );
+}
---
+++ features/marketing/heroes.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+
+export function Heroes() {
+  return (
+    <div className="flex flex-col items-center justify-center max-w-5xl">
+      <div className="flex items-center">
+        <div className="relative w-75 h-75 sm:w-87.5 sm:h-87.5 md:h-100 md:w-100">
+          <Image
+            src={"/documents.webp"}
+            fill
+            className="object-contain"
+            alt="Documents"
+          />
+        </div>
+        <div className="relative h-100 w-100 hidden md:block">
+          <Image
+            src={"/reading.webp"}
+            alt="Reading"
+            fill
+            className="object-contain"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
