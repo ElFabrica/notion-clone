0d6a48f1f2df3ea8b4e7d17aab4434b5d8b91607
Arthur Fabricyo <142946955+ElFabrica@users.noreply.github.com>
2026-03-13 23:11:53 +0000

    feat/update navigation

--- features/main/components/navigation.tsx
+++ features/main/components/navigation.tsx
@@ -1,15 +1,31 @@
 import { ChevronsLeftIcon } from "lucide-react";
+import { usePathname } from "next/navigation";
+import { useRef, ElementRef, useState } from "react";
+import { useMediaQuery } from "usehooks-ts";
+import { cn } from "@/lib/utils";
 
 export function Navigation() {
+  const pathname = usePathname();
+  const isMobile = useMediaQuery("(max-width: 768)");
+  const isResizingRef = useRef(false);
+  const sidebarRef = useRef<ElementRef<"aside">>(null);
+  const navBaRef = useRef<ElementRef<"div">>(null);
+  const [isResetting, setIsResetting] = useState(false);
+  const [isCollapsed, setIsCollapsed] = useState(isMobile);
+
   return (
     <>
       <aside
-        className={
-          "group/sidebar h-full bg-secoundary overflow-y-auto relative flex w-60 flex-col z-99999"
-        }
+        ref={sidebarRef}
+        className={cn(
+          "group/sidebar h-full bg-secoundary overflow-y-auto relative flex w-60 flex-col z-99999",
+        )}
       >
-        <div role="button" className="">
-          <ChevronsLeftIcon className="h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 absolute top-3 right-3 opacity-0" />
+        <div
+          role="button"
+          className="text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 absolute top-3 right-3 opacity-0 group-hover/sidebar:opacity-100 transition"
+        >
+          <ChevronsLeftIcon className="h-6 w-6 " />
         </div>
         <div>
           <p>Action items</p>
