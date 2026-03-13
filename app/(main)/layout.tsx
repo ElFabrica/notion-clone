86cf2cca0ae5f3f6c41a8484b587072d50d1ff9a
Arthur Fabricyo <142946955+ElFabrica@users.noreply.github.com>
2026-03-12 21:19:54 -0300

    feat/udating to reload

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -3,6 +3,7 @@
 import { Spinner } from "@/components/ui/spinner";
 import { useConvexAuth } from "convex/react";
 import { redirect } from "next/navigation";
+import {Navigation} from "@features/main/components/navigation"
 
 export default function LayoutPage({
   children,
@@ -21,6 +22,7 @@ export default function LayoutPage({
   }
   return (
     <div className={"h-full flex dark:bg-[#1f1f1f] "}>
+      <Navigation
       <main className="flex-1 h-full overflow-y-auto">{children}</main>
     </div>
   );
