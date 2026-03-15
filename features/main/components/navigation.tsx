e9d49e9a684db0d50e7fe658880913de15022e71
Arthur Fabricyo <arthur.fabricyo@gmail.com>
2026-03-15 00:21:19 -0300

    git add .

--- features/main/components/navigation.tsx
+++ features/main/components/navigation.tsx
@@ -1,6 +1,6 @@
-import { ChevronsLeftIcon } from "lucide-react";
+import { ChevronsLeftIcon, MenuIcon } from "lucide-react";
 import { usePathname } from "next/navigation";
-import { useRef, ElementRef, useState } from "react";
+import React, { useRef, ElementRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
 import { cn } from "@/lib/utils";
 
@@ -13,17 +13,72 @@ export function Navigation() {
   const [isResetting, setIsResetting] = useState(false);
   const [isCollapsed, setIsCollapsed] = useState(isMobile);
 
+  const handleMouseDown = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>,
+  ) => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    isResizingRef.current = true;
+    document.addEventListener("mouseenter", handleMouseMove);
+    document.addEventListener("mouseup", handleMouseUp);
+  };
+
+  const handleMouseMove = (event: MouseEvent) => {
+    if (isResizingRef.current) return;
+    let newWidth = event.clientX;
+
+    if (newWidth < 240) newWidth = 240;
+    if (newWidth > 480) newWidth = 480;
+
+    if (sidebarRef.current && navBaRef.current) {
+      sidebarRef.current.style.width = `${newWidth}px`;
+      sidebarRef.current.style.setProperty("left", `${newWidth}`);
+      navBaRef.current.style.setProperty("width", `calc(100%-$${newWidth}px)`);
+    }
+  };
+
+  const handleMouseUp = () => {
+    isResizingRef.current = false;
+    document.addEventListener("mousedown", handleMouseMove);
+    document.addEventListener("mouseup", handleMouseUp);
+  };
+
+  const resetWidth = () => {
+    if (sidebarRef.current && navBaRef.current) {
+      setIsCollapsed(false);
+      setIsResetting(false);
+
+      sidebarRef.current.style.width = isMobile ? "100%" : "240px";
+      navBaRef.current.style.setProperty(
+        "width",
+        isMobile ? "0" : "calc(100 - 240px)",
+      );
+
+      navBaRef.current.style.setProperty("left", isMobile ? "100%" : "240px");
+    }
+
+    setTimeout(() => {
+      (setIsResetting(false), 300);
+    });
+  };
+
   return (
     <>
       <aside
         ref={sidebarRef}
         className={cn(
           "group/sidebar h-full bg-secoundary overflow-y-auto relative flex w-60 flex-col z-99999",
+          isResetting && "transition-all ease-in-out duration-300",
+          isMobile && "w-0",
         )}
       >
         <div
           role="button"
-          className="text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 absolute top-3 right-3 opacity-0 group-hover/sidebar:opacity-100 transition"
+          className={cn(
+            "text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 absolute top-3 right-3 opacity-0 group-hover/sidebar:opacity-100 transition",
+            isMobile && "opacity-100 ",
+          )}
         >
           <ChevronsLeftIcon className="h-6 w-6 " />
         </div>
@@ -34,11 +89,27 @@ export function Navigation() {
           <p>Documents</p>
         </div>
         <div
+          onMouseDown={handleMouseDown}
+          onClick={() => {}}
           className={
             "opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0"
           }
         />
       </aside>
+      <div
+        ref={navBaRef}
+        className={cn(
+          "absolute top-0 z-999999 left-60 w-[calc(100%-240px)]",
+          isResetting && "transition-all ease-in-out duration-300",
+          isMobile && "left-0 w-full",
+        )}
+      >
+        <nav className="bg-transparent px-3 py-2 w-full">
+          {isCollapsed && (
+            <MenuIcon role="button" className="size-6 text-muted-foreground" />
+          )}
+        </nav>
+      </div>
     </>
   );
 }
