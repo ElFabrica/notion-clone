9d52e14765af8446bd187075336c398614e2226e
Arthur Fabricyo <142946955+ElFabrica@users.noreply.github.com>
2026-03-16 23:22:52 +0000

    fix/maybe last bug of sidebar

--- features/main/components/navigation.tsx
+++ features/main/components/navigation.tsx
@@ -33,7 +33,7 @@ export function Navigation() {
 
     if (sidebarRef.current && navBarRef.current) {
       sidebarRef.current.style.width = `${newWidth}px`;
-      sidebarRef.current.style.setProperty("left", `${newWidth}px`);
+      navBarRef.current.style.setProperty("left", `${newWidth}px`);
       navBarRef.current.style.setProperty("width", `calc(100%-${newWidth}px)`);
     }
   };
