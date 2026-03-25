db2e306ed25c3b0ab3e451736d11d3ebb4c02db2
ElFabrica <arthur.fabricyo@email.com>
2026-03-25 09:05:17 -0300

    feature/menu initialized

--- features/main/components/navbar.tsx
+++ features/main/components/navbar.tsx
@@ -7,6 +7,7 @@ import { MenuIcon } from "lucide-react";
 import { useParams } from "next/navigation";
 import { Title } from "./title";
 import { Banner } from "./banner";
+import { Menu } from "./menu";
 
 interface NavBarProps {
   isCollapsed: boolean;
@@ -48,6 +49,9 @@ export function NavBar({ isCollapsed, onResetWith }: NavBarProps) {
         )}
         <div className="flex items-center justify-between w-full">
           <Title initialData={document} />
+          <div>
+            <Menu documentId={document._id} />
+          </div>
         </div>
       </nav>
       {document.isArchived && <Banner documentId={document._id} />}
